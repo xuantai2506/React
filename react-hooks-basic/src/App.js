2f86c482a2a6be4566e08ad2a8a726ca20e3cb05
xuantai2506 <xuantai59@gmail.com>
2021-01-15 14:30:51 +0700

    react-hook-basic

---
+++ react-hooks-basic/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h5>Hello xuantai hooks</h5>
+    </div>
+  );
+}
+
+export default App;
